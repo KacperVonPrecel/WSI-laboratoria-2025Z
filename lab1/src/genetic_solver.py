ec178bb98cd341d193e6d26b19938349ec96e545
Kacper Skrodzki <kacskrogry@gmail.com>
2025-10-12 17:56:05 +0200

    Removed varibles 'size_pop' and 'population' from GeneticSolver. Created selection() method. Update evaluate() an find_best(). Moved create_first_population() out of GeneticSolver.

--- lab1/src/genetic_solver.py
+++ lab1/src/genetic_solver.py
@@ -1,59 +1,66 @@
 from solver import Solver
-from calc_target import calc_target, calc_path
+# from calc_target import calc_target, calc_path
 import numpy as np
 from typing import Any, Callable, Dict, Tuple
+
+INPUTS_NUM = 400
 
 
 class GeneticSolver(Solver):
     """A genetic solver with mutation, proportional reproduction,
     one-point crossover and generation succession"""
 
-    def __init__(self, n: int = 50, mutation_p: float = 0.05, crossover_p: float = 0.7, time_budget: int = 50000):
+    def __init__(self, mutation_p: float = 0.05, crossover_p: float = 0.7, time_budget: int = 50000):
         super().__init__()
-        # Population size
-        self.pop_size = n
         # Mutation probability
         self.mutation_p = mutation_p
         # Crossover probability
         self.crossover_p = crossover_p
-        # Current population
-        self.population: np.array[int] = np.zeros((self.pop_size, 200, 2))
         # FES for this solver
         self.time_budget = time_budget
 
     def get_parameters(self) -> Dict[str, Any]:
         param_dict = {
-            "population size": self.pop_size,
             "mutation probability": self.mutation_p,
             "crossover probabiity": self.crossover_p,
             "FES": self.time_budget
         }
         return param_dict
 
-    def create_first_population(self) -> None:
-        for specimen in self.population:
-            t = 0
-            while t < len(specimen):
-                specimen[..., t, 0] = np.random.randint(2)
-                specimen[..., t, 1] = np.random.randint(2)
-                t += 1
-
-    def evaluate(self, func: Callable[[np.ndarray], float]) -> np.ndarray[float]:
-        values = np.zeros(self.pop_size)
-        for i in self.population:
-            values[i] = func(i)
+    def evaluate(self, func: Callable[[np.ndarray], float], population: np.ndarray[int]) -> np.ndarray[float]:
+        values = np.zeros(len(population))
+        pos = 0
+        for i in population:
+            values[pos] = func(i)
+            pos += 1
         return values
 
-    def find_best(self, values: np.ndarray[float]) -> Tuple[np.ndarray, float]:
+    def find_best(self, values: np.ndarray[float], population: np.ndarray[int]) -> Tuple[np.ndarray, float]:
         best = np.argmax(values)
-        return self.population[best], values[best]
+        return population[best], values[best]
 
-    def selection(self, values: np.ndarray[float]):
+    def selection(self, values: np.ndarray[float], population: np.ndarray[int]) -> np.ndarray[int]:
         eps = 1e-9
+        selected_candidates = np.zeros((len(population), INPUTS_NUM))
+        pos = 0
         shifted = values - np.min(values) + eps
         probabilities = shifted / np.sum(shifted)
         # Population after selection
-        self.population = np.random.choice(len(values), size=self.pop_size, p=probabilities)
+        selected_pos = np.random.choice(len(values), size=len(population), p=probabilities)
+        for i in selected_pos:
+            selected_candidates[pos] = population[i]
+            pos += 1
+        return selected_candidates
 
     def solve(self, func: Callable[[np.ndarray], int]) -> Tuple[np.ndarray, float]:
         return super().solve()
+
+
+def create_first_population(pop_size: int = 50) -> np.ndarray[int]:
+    first_population = np.zeros((pop_size, INPUTS_NUM))
+    for specimen in first_population:
+        t = 0
+        while t < len(specimen):
+            specimen[t] = np.random.randint(2)
+            t += 1
+    return first_population
--- lab1/src/main.py
+++ lab1/src/main.py
@@ -3,8 +3,13 @@ from calc_target import calc_target
 
 if __name__ == '__main__':
     test_solver = gen_sol.GeneticSolver()
-    test_solver.create_first_population()
-    print(test_solver.get_parameters())
-    print(test_solver.evaluate(calc_target))
-
-# TO DO: Format osobnika jest zły, przeanalizuj calc_path
+    test_population = gen_sol.create_first_population(20)
+    test_value = test_solver.evaluate(calc_target, test_population)
+    print(test_value)
+    print("\n")
+    print(test_solver.find_best(test_value, test_population))
+    print("\n")
+    test_selection = test_solver.selection(test_value, test_population)
+    test_value = test_solver.evaluate(calc_target, test_selection)
+    print(test_solver.find_best(test_value, test_selection))
+    print("\n")
