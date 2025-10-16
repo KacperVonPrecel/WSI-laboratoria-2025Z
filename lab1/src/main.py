314f9201bcc98723b27501a4d32cc7494a8a8391
Kacper Skrodzki <kacskrogry@gmail.com>
2025-10-16 22:38:55 +0200

    Writen functions for saving base to the file and calculating mean and median from it.

--- lab1/src/main.py
+++ lab1/src/main.py
@@ -1,5 +1,27 @@
 import genetic_solver as gen_sol
 from calc_target import calc_target
+import numpy as np
+
+
+def checking_loop(solver: gen_sol.GeneticSolver, pop_size: int, file_name: str) -> str:
+    test_population = gen_sol.create_first_population(pop_size)
+    with open(file_name, "w") as file_handler:
+        for _ in range(10):
+            _, best_value = solver.solve(calc_target, test_population)
+            print(f"Best value: {best_value}")
+            file_handler.write(f"{str(best_value)} \n")
+    return "Done"
+
+
+def calc_median_and_deviation(file: str) -> tuple[float, float]:
+    with open(file, "r") as fh:
+        for line in fh:
+            values_table = line.strip()
+        values_array = np.array(values_table, dtype=np.float64)
+    median = np.mean(values_array)
+    deviation = np.std(values_array, dtype=np.float64)
+    return median, deviation
+
 
 if __name__ == '__main__':
     print("Probability of mutation (0.0-1.0): ")
@@ -10,9 +32,19 @@ if __name__ == '__main__':
     pop_size = int(input())
     print("Time budget - how many evaluations (integer): ")
     time_bud = int(input())
-    print("Searching...")
-
     test_solver = gen_sol.GeneticSolver(mutation_p=mutation_prob, crossover_p=crossover_prob, time_budget=time_bud)
-    test_population = gen_sol.create_first_population(pop_size)
-    best_specimen, best_value = test_solver.solve(calc_target, test_population)
-    print(f"Best specimen: {best_specimen} \n Best value: {best_value} \n")
+
+    print("Process with the loop? (Y/N): ")
+    decision = str(input())
+    if decision == "Y":
+        print("Input the name of the file to save to:")
+        file_name = str(input())
+        print("Searching...")
+        print(checking_loop(test_solver, pop_size, file_name))
+        print(test_solver.get_parameters())
+        print(calc_median_and_deviation(file_name))
+    else:
+        test_population = gen_sol.create_first_population(pop_size)
+        best_specimen, best_value = test_solver.solve(calc_target, test_population)
+        print(f"Best specimen: {best_specimen} \n Best value: {best_value}")
+        print("Oh. Okay.")
