430c7e13d502e0662b0e68b0c65a6644b8dd6644
Kacper Skrodzki <kacskrogry@gmail.com>
2025-10-18 17:14:59 +0200

    Working on boxplot create function.

--- lab1/src/plot_generator.py
+++ lab1/src/plot_generator.py
@@ -4,24 +4,23 @@ from typing import Any, Callable, Dict, Tuple
 
 
 def box_plot_generation(file_handle):
-    for line in file_handle:
-        source_table = line.strip().split()
-    data_table = np.array(source_table[:-1], dtype=np.float64)
+    source_table = [[float(x) for x in line.split()] for line in file_handle if line.strip()]
+    data_table = source_table[:-1]
     axis_x_labels = source_table[-1]
 
     fig = plt.figure(figsize=(10, 7))
     ax = fig.add_subplot(111)
+    ax.set_xticklabels(axis_x_labels)
+    # ax.get_xaxis().tick_bottom()
+    # ax.get_yaxis().tick_left()
+
     bp = ax.boxplot(data_table)
 
-    ax.set_xticklabels(axis_x_labels)
     plt.title("Testowy boxplot")
-
-    ax.get_xaxis().tick_bottom()
-    ax.get_yaxis().tick_left()
-
+    plt.savefig("Testowy boxplot")
     plt.show()
 
 
 if __name__ == "__main__":
-    with open('test_boxplot.txt', 'r') as f_handle:
+    with open('test_boxplot_data.txt', 'r') as f_handle:
         box_plot_generation(f_handle)
