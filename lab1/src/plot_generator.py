f91ef1d549117e64f7623b6fb8ee9b3f93d4b579
Kacper Skrodzki <kacskrogry@gmail.com>
2025-10-18 18:50:22 +0200

    Finalized box_plot_generation

--- lab1/src/plot_generator.py
+++ lab1/src/plot_generator.py
@@ -3,7 +3,7 @@ import numpy as np
 from typing import Any, Callable, Dict, Tuple
 
 
-def box_plot_generation(file_handle):
+def box_plot_generation(file_handle, tested_parameter: str, plots_title: str):
     source_table = [[float(x) for x in line.split()] for line in file_handle if line.strip()]
     data_table = source_table[:-1]
     axis_x_labels = source_table[-1]
@@ -11,16 +11,28 @@ def box_plot_generation(file_handle):
     fig = plt.figure(figsize=(10, 7))
     ax = fig.add_subplot(111)
     ax.set_xticklabels(axis_x_labels)
-    # ax.get_xaxis().tick_bottom()
-    # ax.get_yaxis().tick_left()
+    ax.set_xlabel(tested_parameter, fontsize=18)
+    ax.set_ylabel("Values", rotation=90, fontsize=18)
 
-    bp = ax.boxplot(data_table)
+    bp = ax.boxplot(data_table, patch_artist=True, notch=False, vert=1)
 
-    plt.title("Testowy boxplot")
-    plt.savefig("Testowy boxplot")
+    for patch in bp['boxes']:
+        patch.set_facecolor("#173CB6")
+
+    for median in bp['medians']:
+        median.set(color= "#E60606", linewidth = 4)
+
+    plt.title(plots_title, fontweight='bold', fontsize=32)
+    plt.savefig(plots_title)
     plt.show()
 
 
 if __name__ == "__main__":
-    with open('test_boxplot_data.txt', 'r') as f_handle:
-        box_plot_generation(f_handle)
+    print("Please input a file's name from which the boxplot will be created:")
+    file_name = str(input())
+    print("Please enter the name of tested parameter:")
+    param_name = str(input())
+    print("Please enter the title of the plot:")
+    title = str(input())
+    with open(file_name, 'r') as f_handle:
+        box_plot_generation(f_handle, param_name, title)
