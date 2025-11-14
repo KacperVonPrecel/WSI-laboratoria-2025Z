69cd7cf39501a3b357b5c7e07be5db0207b2fbce
Kacper Skordzki <01197524@pw.edu.pl>
2025-11-14 22:01:06 +0100

    laid fundations for ID3

---
+++ lab3/src/id3_algorithm.py
@@ -0,0 +1,21 @@
+from solver import Solver
+
+
+class DecisionSolver(Solver):
+    # def __init__(self, attributes, classes, depth):
+    #     self._attributes = attributes
+    #     self._classes = classes
+    #     self._max_depth = depth
+
+    def get_parameters(self):
+        pass
+
+    def fit(self, X, y):
+        pass
+
+    def predict(self, X):
+        pass
+
+    def id3(self, X, y, max_depth):
+        pass
+
