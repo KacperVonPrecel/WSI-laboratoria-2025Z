ccbb6a5db067fd1cf3fb1a73d540a13af4536288
Kacper Skrodzki <kacskrogry@gmail.com>
2026-01-03 13:35:27 +0100

    Added readear for data

---
+++ lab5/src/data_reader.py
@@ -0,0 +1,50 @@
+import pandas as pd
+import random
+from sklearn.model_selection import train_test_split
+
+
+class DataReader():
+
+    def __init__(self):
+        self._train_df = []
+        self._val_df = []
+        self._test_df = []
+
+    def get_train_df(self):
+        return self._train_df
+
+    def get_test_df(self):
+        return self._test_df
+
+    def get_val_df(self):
+        return self._val_df
+
+    def read_data(self, file_handle, rand_seed):
+        df = pd.read_csv(file_handle, sep=",")
+
+        for col in ["fixed_acidity", "volatile_acidity", "citric_acid", "residual_sugar", "chlorides",
+                    "free_sulfur_dioxide", "total_sulfur_dioxide", "density", "pH", "sulphates", "alcohol"]:
+            df[col].fillna(df[col].median(), inplace=True)
+            df[col] = pd.qcut(df[col], q=4, labels=["low", "medium", "high", "very_high"], duplicates='drop')
+
+        self._train_df, temp_df = train_test_split(df, test_size=0.30, random_state=rand_seed, shuffle=True)
+
+        self._val_df, self._test_df = train_test_split(temp_df, test_size=0.50, random_state=rand_seed, shuffle=True)
+
+
+def get_class_values(data_frame):
+    return data_frame["quality"].tolist()
+
+
+def get_labels_values(data_frame):
+    return data_frame.drop(columns=["quality"]).tolist()
+
+
+# if __name__ == "__main__":
+#     reader = DataReader()
+#     r_seed = random.seed("1234")
+#     with open("./wsi5-25Z_dataset.csv") as file_h:
+#         reader.read_data(file_h, r_seed)
+#         print("Train:", reader.get_train_df().head())
+#         y_values = reader.get_train_df()["quality"].tolist()
+#         print("Y values:", y_values)
