caebef0694e2d10285d10efd91bbea818c85f557
Kacper Skrodzki <kacskrogry@gmail.com>
2026-01-03 20:43:38 +0100

    Added SoftmaxCrossEntropy

--- lab5/src/mlp.py
+++ lab5/src/mlp.py
@@ -40,6 +40,24 @@ class DenseLayer:
         return grad_input
 
 
+class SoftmaxCrossEntropy:
+    EPSILON = 1e-9
+
+    def forward(self, logits, y_true_one):
+        exps = np.exp(logits - np.max(logits, axis=1, keepdims=True))
+        probabilities = exps / np.sum(exps, axis=1, keepdims=True)
+        self.probs = probabilities
+        self.y_true = y_true_one
+
+        m = y_true_one.shape[0]
+        loss = -np.sum(y_true_one * np.log(probabilities + self.EPSILON)) / m
+        return loss
+
+    def backward(self):
+        m = self.y_true.shape[0]
+        return (self.probs - self.y_true) / m
+
+
 class MLP:
     def __init__(self):
         self.layers = []
